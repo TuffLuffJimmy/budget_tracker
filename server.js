c89f4c9eb474817d44e3184533c66caf413ff8d6
Jimmy Echternach <jimmynechternach@gmail.com>
2020-06-16 08:57:31 -0700

    preparing for deployment

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const logger = require("morgan");
 const mongoose = require("mongoose");
 const compression = require("compression");
 
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 const app = express();
 
@@ -15,7 +15,8 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-mongoose.connect("mongodb://localhost/budget", {
+// mongodb://localhost/budget
+mongoose.connect("mongodb://heroku_gh89tt66:32o2jmtm150pst62lf2qeeijq1@ds053964.mlab.com:53964/heroku_gh89tt66", {
   useNewUrlParser: true,
   useFindAndModify: false
 });
